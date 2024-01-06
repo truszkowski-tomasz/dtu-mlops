eecf9447ae9cd4c1b33612c4b727ec38443cf5f9
Laura Paz <laurapaz@pop-os.localdomain>
2024-01-06 12:40:26 +0100

    Updated make_dataset to split into train and validation

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,6 +3,7 @@ import zipfile
 from typing import Union
 import pandas as pd
 from kaggle.api.kaggle_api_extended import KaggleApi
+from sklearn.model_selection import train_test_split
 
 def download_dataset(dataset_name: str, destination_path: str) -> None:
     """
@@ -39,13 +40,23 @@ def process_data(input_path: str, output_path: str) -> None:
     # Load the CSV file
     df = pd.read_csv(os.path.join(input_path, 'WELFake_Dataset.csv'))
 
+    # remove the first column (index)
+    df.drop(df.columns[0], axis=1, inplace=True)
+
     # TODO: Data processing logic
     # Embeddings stuff
 
-    # Save the processed data
-    processed_path = os.path.join(output_path, 'processed_data.csv')
-    df.to_csv(processed_path, index=False)
+    # Split into train and validation
+    train_df, val_df = train_test_split(df, test_size=0.2, random_state=42)
 
+    # Save the train and validation data
+    train_path = os.path.join(output_path, 'train_data.csv')
+    val_path = os.path.join(output_path, 'val_data.csv')
+
+    train_df.to_csv(train_path, index=False)
+    val_df.to_csv(val_path, index=False)
+
+# TODO: add hydra
 def main() -> None:
     """
     Main function to download and process the Kaggle dataset.
