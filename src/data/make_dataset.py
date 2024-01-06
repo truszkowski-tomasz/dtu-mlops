94704ac0cebe39a7c401d1191029280213bf930b
Laura Paz <laurapaz@pop-os.localdomain>
2024-01-06 13:38:56 +0100

    Remove kaggle logic, after setting up dvc

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,6 @@
 import os
-import zipfile
-from typing import Union
 import pandas as pd
-from kaggle.api.kaggle_api_extended import KaggleApi
 from sklearn.model_selection import train_test_split
-
-def download_dataset(dataset_name: str, destination_path: str) -> None:
-    """
-    Download the Kaggle dataset using the Kaggle API.
-
-    Parameters:
-        dataset_name (str): The name of the Kaggle dataset (e.g., 'username/dataset').
-        destination_path (str): The local path where the dataset files will be downloaded.
-
-    Returns:
-        None
-    """
-    api = KaggleApi()
-    # TODO: Add instructions to get the Kaggle API token in the README
-    api.authenticate()
-
-    # Download dataset
-    api.dataset_download_files(dataset_name, path=destination_path)
-
-    with zipfile.ZipFile(os.path.join(destination_path, dataset_name.split('/')[1] + '.zip'), 'r') as zip_ref:
-        zip_ref.extractall(destination_path)
 
 def process_data(input_path: str, output_path: str) -> None:
     """
@@ -59,18 +35,13 @@ def process_data(input_path: str, output_path: str) -> None:
 # TODO: add hydra
 def main() -> None:
     """
-    Main function to download and process the Kaggle dataset.
+    Main function to process the dataset.
 
     Returns:
         None
     """
-    # TODO: dataset_name should be in a config file
     # TODO: raw_path should be in a config file
-    dataset_name = 'saurabhshahane/fake-news-classification'
     raw_path = 'data/raw'
-
-    download_dataset(dataset_name, raw_path)
-
     # TODO: processed_path should be in a config file
     processed_path = 'data/processed'
     process_data(raw_path, processed_path)
