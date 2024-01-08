87703f7e8a8b8eb0fb85b7fe7703cd3e2fe7a488
truszkowski-tomasz <79571804+truszkowski-tomasz@users.noreply.github.com>
2024-01-08 16:57:14 +0100

    update model

--- src/models/model.py
+++ src/models/model.py
@@ -4,12 +4,12 @@ import transformers
 class BERTClass(torch.nn.Module):
     def __init__(self):
         super(BERTClass, self).__init__()
-        self.l1 = transformers.BertModel.from_pretrained('bert-base-uncased')
+        self.l1 = transformers.BertModel.from_pretrained('models/bert-base-uncased') # We are using downloaded model
         self.l2 = torch.nn.Dropout(0.3)
-        self.l3 = torch.nn.Linear(768, 1)
+        self.l3 = torch.nn.Linear(768, 1)  # Change the output size to 1
     
     def forward(self, ids, mask, token_type_ids):
         _, output_1 = self.l1(ids, attention_mask=mask, token_type_ids=token_type_ids, return_dict=False)
         output_2 = self.l2(output_1)
-        output = self.l3(output_2).squeeze(1) 
+        output = self.l3(output_2)
         return output
