5b0399c859ba6a683782756af9f8298c5d0c58da
truszkowski-tomasz <79571804+truszkowski-tomasz@users.noreply.github.com>
2024-01-09 17:46:46 +0100

    logger

---
+++ src/utils/logger.py
@@ -0,0 +1,50 @@
+import logging
+import logging.config
+from pathlib import Path
+import sys
+
+LOGS_DIR = "logs"
+
+logging_config = {
+    "version": 1,
+    "formatters": {
+        "minimal": {"format": "%(message)s"},
+        "detailed": {
+            "format": "%(levelname)s %(asctime)s [%(name)s:%(filename)s:%(funcName)s:%(lineno)d]\n%(message)s\n"
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "stream": sys.stdout,
+            "formatter": "minimal",
+            "level": logging.DEBUG,
+        },
+        "info": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": Path(LOGS_DIR, "info.log"),
+            "maxBytes": 10485760,  # 1 MB
+            "backupCount": 10,
+            "formatter": "detailed",
+            "level": logging.INFO,
+        },
+        "error": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": Path(LOGS_DIR, "error.log"),
+            "maxBytes": 10485760,  # 1 MB
+            "backupCount": 10,
+            "formatter": "detailed",
+            "level": logging.ERROR,
+        },
+    },
+    "root": {
+        "handlers": ["console", "info", "error"],
+        "level": logging.INFO,
+        "propagate": True,
+    },
+}
+
+logging.config.dictConfig(logging_config)
+
+def get_logger(name):
+    return logging.getLogger(name)
