6193eaa5e5ccdb65d6d25947fba71ddee5593824
truszkowski-tomasz <79571804+truszkowski-tomasz@users.noreply.github.com>
2024-01-11 12:53:12 +0100

    refactor

--- src/train_model.py
+++ src/train_model.py
@@ -1,16 +1,14 @@
+import os
+import random
 import numpy as np
 import pandas as pd
 from sklearn import metrics
 import torch
 from torch.utils.data import DataLoader
-from models.model import BERTClass
-import matplotlib.pyplot as plt
-import random
-from torch import cuda
-from utils.logger import get_logger
 from tqdm import tqdm
 import wandb
-import os
+from models.model import BERTClass
+from utils.logger import get_logger
 
 logger = get_logger(__name__)
 
@@ -21,7 +19,6 @@ LEARNING_RATE = 1e-05
 EPOCHS = 3
 
 config = {"train_batch_size": TRAIN_BATCH_SIZE, "valid_batch_size": VALID_BATCH_SIZE, "epochs": EPOCHS}
-
 wandb.init(project="dtu-mlops", config=config)
 
 # Set a random seed for reproducibility
@@ -33,6 +30,7 @@ random.seed(random_seed)
 # Constants and parameters
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+# Load datasets
 train_set = torch.load("data/processed/train_set.pt")
 val_set = torch.load("data/processed/val_set.pt")
 
@@ -40,31 +38,22 @@ val_set = torch.load("data/processed/val_set.pt")
 train_loader = DataLoader(train_set, batch_size=TRAIN_BATCH_SIZE, shuffle=True, num_workers=0)
 val_loader = DataLoader(val_set, batch_size=VALID_BATCH_SIZE, shuffle=True, num_workers=0)
 
-# Initializing the model, loss function, and optimizer
-model = BERTClass()
-model.to(device)
+# Initialize the model, loss function, and optimizer
+model = BERTClass().to(device)
 wandb.watch(model, log_freq=100)
 
 loss_fn = torch.nn.BCEWithLogitsLoss()
 optimizer = torch.optim.Adam(params=model.parameters(), lr=LEARNING_RATE)
 
 # Training loop
-train_losses = []
-val_losses = []
+train_losses, val_losses = [], []
 
 for epoch in range(EPOCHS):
+    # Training
     model.train()
     total_loss = 0
-    # Wrap the train_loader with tqdm for progress visualization
-    train_loader_iter = tqdm(enumerate(train_loader, 0), total=len(train_loader))
-    for _, data in train_loader_iter:
-        ids, mask, token_type_ids, targets = data
-        ids, mask, token_type_ids, targets = (
-            ids.to(device),
-            mask.to(device),
-            token_type_ids.to(device),
-            targets.to(device),
-        )
+    for _, data in tqdm(enumerate(train_loader, 0), total=len(train_loader), desc=f'Epoch {epoch} - Training'):
+        ids, mask, token_type_ids, targets = [d.to(device) for d in data]
 
         optimizer.zero_grad()
         outputs = model(ids, mask, token_type_ids)
@@ -78,33 +67,25 @@ for epoch in range(EPOCHS):
     average_loss = total_loss / len(train_loader)
     train_losses.append(average_loss)
 
+    # Validation
     model.eval()
     total_val_loss = 0
-    fin_val_targets = []
-    fin_val_outputs = []
-    # Wrap the val_loader with tqdm for progress visualization
-    val_loader_iter = tqdm(enumerate(val_loader, 0), total=len(val_loader))
-    with torch.no_grad():
-        for _, data in val_loader_iter:
-            ids, mask, token_type_ids, targets = data
-            ids, mask, token_type_ids, targets = (
-                ids.to(device),
-                mask.to(device),
-                token_type_ids.to(device),
-                targets.to(device),
-            )
+    fin_val_targets, fin_val_outputs = [], []
+    for _, data in tqdm(enumerate(val_loader, 0), total=len(val_loader), desc=f'Epoch {epoch} - Validation'):
+        ids, mask, token_type_ids, targets = [d.to(device) for d in data]
 
-            outputs = model(ids, mask, token_type_ids)
+        outputs = model(ids, mask, token_type_ids)
 
-            val_loss = loss_fn(outputs, targets)
-            total_val_loss += val_loss.item()
+        val_loss = loss_fn(outputs, targets)
+        total_val_loss += val_loss.item()
 
-            fin_val_targets.extend(targets.cpu().detach().numpy().tolist())
-            fin_val_outputs.extend(torch.sigmoid(outputs).cpu().detach().numpy().tolist())
+        fin_val_targets.extend(targets.cpu().detach().numpy().tolist())
+        fin_val_outputs.extend(torch.sigmoid(outputs).cpu().detach().numpy().tolist())
 
     average_val_loss = total_val_loss / len(val_loader)
     val_losses.append(average_val_loss)
 
+    # Metrics and logging
     val_outputs = np.array(fin_val_outputs) >= 0.5
     accuracy = metrics.accuracy_score(fin_val_targets, val_outputs)
     f1_score_micro = metrics.f1_score(fin_val_targets, val_outputs, average="micro")
@@ -114,15 +95,10 @@ for epoch in range(EPOCHS):
     logger.info(f"  Training Loss = {average_loss}")
     logger.info(f"  Validation Loss = {average_val_loss}")
 
-    wandb.log({"Training Loss": average_loss})
-    wandb.log({"Validation Loss": average_val_loss})
-    wandb.log({"Accuracy Score": accuracy})
-    wandb.log({"F1 Score (Micro)": f1_score_micro})
-    wandb.log({"F1 Score (Macro)": f1_score_macro})
+    wandb.log({"Training Loss": average_loss, "Validation Loss": average_val_loss,
+               "Accuracy Score": accuracy, "F1 Score (Micro)": f1_score_micro, "F1 Score (Macro)": f1_score_macro})
 
 # Save the model
 model_path = "models/fine_tuned"
-# Check if the folder exists, and if not, create it
-if not os.path.exists(model_path):
-    os.makedirs(model_path)
+os.makedirs(model_path, exist_ok=True)
 torch.save(model.state_dict(), os.path.join(model_path, "bert_model.pth"))
