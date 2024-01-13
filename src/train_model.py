979b8f6486236e1d33c4bdd3f07f81d847d72f73
Àiax <aiax.developer@gmail.com>
2024-01-13 14:23:22 +0100

    Add pytorch profiling

--- src/train_model.py
+++ src/train_model.py
@@ -1,5 +1,3 @@
-import cProfile
-import pstats
 import os
 import random
 import numpy as np
@@ -11,51 +9,54 @@ from pytorch_lightning.loggers import WandbLogger
 from sklearn import metrics
 from torch.utils.data import DataLoader
 from models.model import BERTLightning
+from torch.profiler import profile, ProfilerActivity, record_function  # Import torch profiler
 
-@profile
-def main():
-    # Set a random seed for reproducibility
-    random_seed = 42
-    torch.manual_seed(random_seed)
-    np.random.seed(random_seed)
-    random.seed(random_seed)
+# Set a random seed for reproducibility
+random_seed = 42
+torch.manual_seed(random_seed)
+np.random.seed(random_seed)
+random.seed(random_seed)
 
-    # Constants and parameters
-    TRAIN_BATCH_SIZE = 32
-    VALID_BATCH_SIZE = 16
-    EPOCHS = 3
-    LEARNING_RATE = 1e-05
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# Constants and parameters
+TRAIN_BATCH_SIZE = 32
+VALID_BATCH_SIZE = 16
+EPOCHS = 3
+LEARNING_RATE = 1e-05
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    config = {"train_batch_size": TRAIN_BATCH_SIZE, "valid_batch_size": VALID_BATCH_SIZE, "epochs": EPOCHS, "lr": LEARNING_RATE}
-    wandb.init(project="dtu-mlops", config=config)
+config = {"train_batch_size": TRAIN_BATCH_SIZE, "valid_batch_size": VALID_BATCH_SIZE, "epochs": EPOCHS, "lr": LEARNING_RATE}
+wandb.init(project="dtu-mlops", config=config)
 
-    # Load datasets
-    train_set = torch.load("data/processed/train_set.pt")
-    val_set = torch.load("data/processed/val_set.pt")
+# Load datasets
+train_set = torch.load("data/processed/train_set.pt")
+val_set = torch.load("data/processed/val_set.pt")
 
-    # Create DataLoader
-    train_loader = DataLoader(train_set, batch_size=TRAIN_BATCH_SIZE, shuffle=True, num_workers=7)
-    val_loader = DataLoader(val_set, batch_size=VALID_BATCH_SIZE, shuffle=False, num_workers=7)
+# Create DataLoader
+train_loader = DataLoader(train_set, batch_size=TRAIN_BATCH_SIZE, shuffle=True, num_workers=7)
+val_loader = DataLoader(val_set, batch_size=VALID_BATCH_SIZE, shuffle=False, num_workers=7)
 
-    # Initializing the model, loss function, and optimizer
-    model = BERTLightning().to(device)
+# Initializing the model, loss function, and optimizer
+model = BERTLightning().to(device)
 
-    wandb.watch(model, log_freq=100)
-    logger = WandbLogger()
+wandb.watch(model, log_freq=100)
+logger = WandbLogger()
 
-    trainer = Trainer(max_epochs=EPOCHS, log_every_n_steps=1, logger=logger)
+trainer = Trainer(max_epochs=EPOCHS, log_every_n_steps=1, logger=logger)
 
-    trainer.fit(model=model, train_dataloaders=train_loader, val_dataloaders=val_loader)
+# Profiling
+with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], record_shapes=True) as prof:
+    with record_function("model_training"):
+        trainer.fit(model=model, train_dataloaders=train_loader, val_dataloaders=val_loader)
 
-    model_path = "models/fine_tuned"
+# Print profiler results
+print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=10))
 
-    # If the directory does not exist, create it
-    if not os.path.exists(model_path):
-        os.mkdir(model_path)
+model_path = "models/fine_tuned"
 
-    # Save the model
-    torch.save(model.state_dict(), model_path+"/bert_model.pth")
+# If the directory does not exist, create it
+if not os.path.exists(model_path):
+    os.mkdir(model_path)
 
-if __name__ == "__main__":
-    main()
+# Save the model
+torch.save(model.state_dict(), model_path+"/bert_model.pth")
+wandb.finish()
