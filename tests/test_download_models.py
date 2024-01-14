0f76fa176161c48b79481cea599089a727d3fe23
truszkowski-tomasz <79571804+truszkowski-tomasz@users.noreply.github.com>
2024-01-14 16:25:34 +0100

    modifying actions

--- tests/test_download_models.py
+++ tests/test_download_models.py
@@ -1,3 +1,7 @@
+import os
+
+from src.data.download_models import download_and_save_model
+
 # @pytest.fixture
 # def save_directory(tmpdir):
 #     return str(tmpdir)
@@ -8,19 +12,19 @@ def test_actions_2():
     assert True
 
 
-# def test_download_and_save_model_existing_directory(save_directory, caplog):
-#     # Create a dummy file in the save directory to simulate an existing model
-#     os.makedirs(save_directory, exist_ok=True)
-#     with open(os.path.join(save_directory, "dummy_model.bin"), "w") as f:
-#         f.write("dummy content")
+def test_download_and_save_model_existing_directory(save_directory, caplog):
+    # Create a dummy file in the save directory to simulate an existing model
+    os.makedirs(save_directory, exist_ok=True)
+    with open(os.path.join(save_directory, "dummy_model.bin"), "w") as f:
+        f.write("dummy content")
 
-#     # TODO: We would like to use mock here!!!
-#     download_and_save_model("dummy_model", save_directory)
+    # TODO: We would like to use mock here!!!
+    download_and_save_model("dummy_model", save_directory)
 
-#     # Check that the function logs a  and returns early
-#     assert "Model 'dummy_model' already exists" in caplog.text
-#     assert not os.path.exists(os.path.join(save_directory, "dummy_model"))
+    # Check that the function logs a  and returns early
+    assert "Model 'dummy_model' already exists" in caplog.text
+    assert not os.path.exists(os.path.join(save_directory, "dummy_model"))
 
-#     # Remove the dummy file and model
-#     os.remove(os.path.join(save_directory, "dummy_model.bin"))
-#     os.rmdir(save_directory)
+    # Remove the dummy file and model
+    os.remove(os.path.join(save_directory, "dummy_model.bin"))
+    os.rmdir(save_directory)
