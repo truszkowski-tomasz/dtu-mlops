38c357edadc9a6b8f085c189e054632af22e35e3
LauraPaz <laura.andrea.paz@gmail.com>
2024-01-17 12:48:39 +0100

    Updated prediction

--- src/app/main.py
+++ src/app/main.py
@@ -8,6 +8,9 @@ uvicorn --reload --port 8000 src.app.main:app
 from fastapi import FastAPI, Form, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse, RedirectResponse
+from src.predict_model import predict
+import pandas as pd
+import hydra
 import time
 
 app = FastAPI()
@@ -21,19 +24,25 @@ async def home(request: Request):
 
 @app.post("/predict/")
 async def predict(request: Request, news: str = Form(...)):
-    time.sleep(2)  # Simulate running process
-    predicted_label = True  # There will be model prediction here
 
-    prediction_message = "REAL!" if predicted_label else "FAKE!"
+    with hydra.initialize(config_path="../config", version_base="1.1"):
+        cfg = hydra.compose(config_name="default_config.yaml", overrides=[f"predict.texts=['{news}']"])
 
-    return templates.TemplateResponse(
-        "result.html",
-        {
-            "request": request,
-            "input_news": news,
-            "prediction": f"Given news is likely to be... {prediction_message}",
-        },
-    )
+        predictions = predict(cfg)
+
+        print(predictions)
+        print(type(predictions))
+
+        prediction_message = "REAL!" if predictions else "FAKE!"
+
+        return templates.TemplateResponse(
+            "result.html",
+            {
+                "request": request,
+                "input_news": news,
+                "prediction": f"Given news is likely to be... {prediction_message}",
+            },
+        )
 
 
 @app.get("/predict/", response_class=RedirectResponse)
--- src/predict_model.py
+++ src/predict_model.py
@@ -1,24 +1,24 @@
 import torch
 import hydra
 import pandas as pd
+import numpy as np
 from omegaconf import DictConfig
-from data.make_dataset import tokenize_and_convert
-from models.model import BERTLightning
+from src.data.make_dataset import tokenize_and_convert
+from src.models.model import BERTLightning
 from pytorch_lightning import Trainer
 from torch.utils.data import DataLoader
 from src.utils.logger import get_logger
 
-@hydra.main(config_path="config", config_name="default_config.yaml", version_base="1.1")
-def predict(config: DictConfig) -> None:
+def predict(cfg: DictConfig) -> np.ndarray:
     """
     Run prediction for a given dataframe
 
     Args:
     ----
+        cfg: DictConfig
+            Configuration object
         df: pd.DataFrame
             Dataframe containing the text to be predicted
-        config: DictConfig
-            Configuration object
     Returns:
     -------
         Tensor of shape [N, d] where N is the number of samples and d is the output dimension of the model
@@ -27,39 +27,39 @@ def predict(config: DictConfig) -> None:
     logger = get_logger(__name__)
 
     # Load the model from the specific checkpoint
-    model = BERTLightning.load_from_checkpoint(config.model.fine_tuned_path + "/bert_model.ckpt", config=config)
+    model = BERTLightning.load_from_checkpoint(cfg.model.fine_tuned_path + "/bert_model.ckpt", config=cfg)
 
-    model.eval()
+    texts = cfg.predict.texts
 
-    # Create a dataframe table with 2 samples, each with title and text
-    df = pd.DataFrame(
-        {
-            "title": [
-                "The best movie I have ever seen",
-                "The worst movie I have ever seen",
-            ],
-            "text": [
-                "This movie was great. The acting was great and the plot was great!",
-                "This movie was bad. The acting was bad and the plot was bad!",
-            ],
-        })
-
-    print(df)
-    print(df.columns)
+    # Create a dataframe with the expected columns
+    df = pd.DataFrame({
+        # title should be a list of empty strings, as long as the number of texts
+        "title": [""] * len(texts),
+        "text": texts,
+    })
 
     # Preprocess the data 
-    tensorDataset = tokenize_and_convert(config.data.max_len, df)
+    tensorDataset = tokenize_and_convert(cfg.data.max_len, df)
 
-    dataloader = DataLoader(tensorDataset, batch_size=10, num_workers=7)
+    dataloader = DataLoader(tensorDataset, batch_size=1, num_workers=7)
 
     trainer = Trainer()
 
     # Run prediction
     predictions = trainer.predict(model, dataloader)
 
+    # Turn the predictions from a list of tensors to a flat numpy array
+    predictions = np.concatenate(predictions).ravel()
+
     logger.info(f"Predictions: {predictions}")
 
     return predictions
 
+@hydra.main(config_path="config", config_name="default_config.yaml", version_base="1.1")
+def main(cfg: DictConfig) -> None:
+
+    # Run prediction
+    predictions = predict(cfg)
+
 if __name__ == "__main__":
-    predictions = predict()
+    main()
