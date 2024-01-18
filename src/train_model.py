9e50ce5c5f1a338cffac1db07dd6cf924d6ccec9
Àiax <aiax.developer@gmail.com>
2024-01-18 15:40:55 +0100

    Make train Vertex AI store model in bucket

--- src/train_model.py
+++ src/train_model.py
@@ -12,6 +12,20 @@ from torch.utils.data import DataLoader
 import wandb
 from models.model import BERTLightning
 
+from google.cloud import storage
+
+def upload_to_bucket(bucket_name, source_file_name, destination_blob_name):
+    """Uploads a file to the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_filename(source_file_name)
+
+    print(
+        f"File {source_file_name} uploaded to {destination_blob_name}."
+    )
+
 
 @hydra.main(config_path="config", config_name="default_config.yaml", version_base="1.1")
 def train(config: DictConfig) -> None:
@@ -56,6 +70,22 @@ def train(config: DictConfig) -> None:
     # Save the model
     torch.save(model.state_dict(), fined_tune_path + "/bert_model.pth")
     trainer.save_checkpoint(fined_tune_path + "/bert_model.ckpt")
+
+    bucket_name = "vertex-ai-fake-news-bucket"  # Replace with your bucket name
+
+    # Paths of local model files
+    local_model_path = fined_tune_path + "/bert_model.pth"
+    local_checkpoint_path = fined_tune_path + "/bert_model.ckpt"
+
+    # Destination paths in the bucket
+    bucket_model_path = "bert_model.pth"
+    bucket_checkpoint_path = "bert_model.ckpt"
+
+    # Upload files
+    upload_to_bucket(bucket_name, local_model_path, bucket_model_path)
+    upload_to_bucket(bucket_name, local_checkpoint_path, bucket_checkpoint_path)
+
+
     wandb.finish()
 
 
